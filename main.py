ba414e842bba83a5b197b3d7889c534fa2483317
RecursiveCat <connor.ueyn@gmail.com>
2021-01-18 15:25:15 -0500

    EXEL demo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 try:
+  import sys
   import modules
   import config
   import vk_api
@@ -7,18 +8,23 @@ except:
 
 try:
   from modules import VK
+  from modules import EXEL
 except:
   print("Error: cannot import program files :(")
 
 try:
   num = config.number
   pwd = config.pwd
+  file_with_table = sys.argv[1]
+  print("Table: "+str(file_with_table))
 except:
   print("Error: cannot use config - invalid or doesnt exists")
 
 try:
   vk = VK.VK(num,pwd)
   vk = vk.GET_API()
+  exel = EXEL.EXEL(file_with_table)
+  print(exel.DUMP_TABLE())
 except:
   print("Error: program entities didnt inits")
 
--- modules/EXEL.py
+++ modules/EXEL.py
@@ -1,1 +1,15 @@
 import pandas
+
+class EXEL:
+  def __init__(self,path_to_table):
+    print(str(__class__)+" : inited")
+    self.FILE = path_to_file
+    try:
+      self.TABLE = pandas.read_csv(self.FILE)
+    except:
+      print(str(__class__)+" : running error : cannot open file " + str(self.FILE))
+  def DUMP_TABLE(self):
+    try:
+      return list(self.TABLE)
+    except:
+      print(str(__class__)+" : error while exporting data from table")
